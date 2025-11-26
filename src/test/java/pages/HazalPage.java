53e87e051be1f0629f863b4fe6fa67bac8bdfaef
hazlsalman <hazlsalman@gmail.com>
2025-11-26 21:54:45 +0300

    Hazal/26.11.2025/21:54

---
+++ src/test/java/pages/HazalPage.java
@@ -0,0 +1,43 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+public class HazalPage {
+
+    public HazalPage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+
+    @FindBy(xpath = "//input[@name='search']")
+    public WebElement homePageAramaKutusu;
+
+    @FindBy (xpath = "(//a[@class='btn_add'])[1]")
+    public WebElement homePageSignInOrProfileButton;
+
+    @FindBy (xpath = "//input[@name='email']")
+    public WebElement loginPageEmailKutusu;
+
+    @FindBy (xpath = "//input[@name='password']")
+    public WebElement loginPagePasswordKutusu;
+
+    @FindBy (xpath = "//button[@type='submit']")
+    public WebElement loginPageSignInButton;
+
+    @FindBy (xpath = "//span[@class='semi-bold']")
+    public WebElement dashboardPageAdminYaziElementi;
+
+    @FindBy (xpath = "//span[@class='thumbnail-wrapper d32 border-5  inline']")
+    public WebElement profilMenuIkonu;
+
+    @FindBy (xpath = "//span[text()='Logout']")
+    public WebElement logoutButton;
+
+
+
+
+
+}
