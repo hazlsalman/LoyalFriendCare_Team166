ca5f93c8104ccb0131eb166bb85d4521b8b576f6
Wadoud101 <wadoud101@gmail.com>
2025-11-27 12:16:57 +0500

    wadoud/27.11.25/10:16

--- src/test/java/pages/WadoudPages.java
+++ src/test/java/pages/WadoudPages.java
@@ -28,6 +28,6 @@ public class WadoudPages {
         public WebElement invalidCredentialTextMessage;
 
         /* @FindBy()
-        public WebElement a; **/
+        public WebElement loginPage; **/
 
 }
