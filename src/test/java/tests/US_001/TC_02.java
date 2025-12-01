86bfbf4f27002b9559d71f5014a124daa4439b2e
gokcenes <enesgokce21@gmail.com>
2025-12-01 18:45:56 +0300

    Enes Page

--- src/test/java/tests/US_001/TC_02.java
+++ src/test/java/tests/US_001/TC_02.java
@@ -1,7 +1,5 @@
 package tests.US_001;
 
-import org.openqa.selenium.WebElement;
-import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.EnesPage;
 import utilities.ConfigReader;
@@ -9,7 +7,7 @@ import utilities.Driver;
 
 public class TC_02 {
     @Test
-    public void islevselHeaderTesti(){
+    public void anaSayfayaErisimTesti(){
         // Ziyaretçi, herhangi bir giriş veya kayıt yapmadan url ile siteye erişim sağlar.
 
 //        Kayıtlı veya yönetici kullanıcı, url ile siteye erişim sağlar.
@@ -19,51 +17,29 @@ public class TC_02 {
 // header kısmındaki logo, sign in, sign up butonu ve Home, About Us,
 // Departments vb. menü ögelerinin görünür olduğunu doğrular.
         EnesPage enesPage =new EnesPage();
-
-        WebElement tumHeaderElement=enesPage.tumHeader;
-        Assert.assertTrue(tumHeaderElement.isDisplayed());
-
-        WebElement headerLogoElement=enesPage.headerLogo;
-        Assert.assertTrue(headerLogoElement.isDisplayed());
-
-        WebElement headerHomeElement=enesPage.headerHome;
-        Assert.assertTrue(headerHomeElement.isDisplayed());
-
-        WebElement headerAboutUsElement=enesPage.headerAboutUs;
-        Assert.assertTrue(headerAboutUsElement.isDisplayed());
-
-        WebElement headerDoctorsMenuElement=enesPage.headerDoctorsMenu;
-        Assert.assertTrue(headerDoctorsMenuElement.isDisplayed());
-
-        WebElement headerDeparmentsMenuElement=enesPage.headerDeparmentsMenu;
-        Assert.assertTrue(headerDeparmentsMenuElement.isDisplayed());
-
-        WebElement headerMedicinesMenuElement=enesPage.headerMedicinesMenu;
-        Assert.assertTrue(headerMedicinesMenuElement.isDisplayed());
-
-        WebElement headerVaccinationsMenuElement=enesPage.headerVaccinationsMenu;
-        Assert.assertTrue(headerVaccinationsMenuElement.isDisplayed());
-
-        WebElement signInButtonElement=enesPage.signInButton;
-        Assert.assertTrue(signInButtonElement.isDisplayed());
-
-        WebElement signUpButtonElement=enesPage.signUpButton;
-        Assert.assertTrue(signUpButtonElement.isDisplayed());
+        enesPage.tumHeader.isDisplayed();
+        enesPage.headerLogo.isDisplayed();
+        enesPage.headerHome.isDisplayed();
+        enesPage.headerAboutUs.isDisplayed();
+        enesPage.headerDoctorsMenu.isDisplayed();
+        enesPage.headerDeparmentsMenu.isDisplayed();
+        enesPage.headerMedicinesMenu.isDisplayed();
+        enesPage.headerVaccinationsMenu.isDisplayed();
+        enesPage.signInButton.isDisplayed();
+        enesPage.signUpButton.isDisplayed();
 
 
         //Kullanıcı (ziyaretçi, kayıtlı, yönetici) header kısmındaki logo, sign in, sign up butonu ve Home, About Us,
         // Departments vb. menü ögelerini tıklanabildiğini doğrular.
-
-      Assert.assertTrue(headerLogoElement.isEnabled());
-      Assert.assertTrue(headerHomeElement.isEnabled());
-      Assert.assertTrue(headerAboutUsElement.isEnabled());
-      Assert.assertTrue(headerDeparmentsMenuElement.isEnabled());
-      Assert.assertTrue(headerDoctorsMenuElement.isEnabled());
-      Assert.assertTrue(headerMedicinesMenuElement.isEnabled());
-      Assert.assertTrue(headerVaccinationsMenuElement.isEnabled());
-      Assert.assertTrue(signInButtonElement.isEnabled());
-      Assert.assertTrue(signUpButtonElement.isEnabled());
-
+        enesPage.headerLogo.isEnabled();
+        enesPage.headerHome.isEnabled();
+        enesPage.headerAboutUs.isEnabled();
+        enesPage.headerDeparmentsMenu.isEnabled();
+        enesPage.headerDoctorsMenu.isEnabled();
+        enesPage.headerMedicinesMenu.isEnabled();
+        enesPage.headerVaccinationsMenu.isEnabled();
+        enesPage.signInButton.isEnabled();
+        enesPage.signUpButton.isEnabled();
 
 
         Driver.quitDriver();
--- src/test/java/tests/US_001/TC_03.java
+++ src/test/java/tests/US_001/TC_03.java
@@ -1,7 +1,5 @@
 package tests.US_001;
 
-import org.openqa.selenium.WebElement;
-import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.EnesPage;
 import utilities.ConfigReader;
@@ -9,7 +7,7 @@ import utilities.Driver;
 
 public class TC_03 {
 @Test
-   public void aramaKutusuTesti(){
+   public void anaSayfayaErisimTesti(){
        // Ziyaretçi, herhangi bir giriş veya kayıt yapmadan url ile siteye erişim sağlar.
 
 //        Kayıtlı veya yönetici kullanıcı, url ile siteye erişim sağlar.
@@ -19,16 +17,10 @@ public class TC_03 {
 
 
        EnesPage enesPage =new EnesPage();
-   WebElement aramaKutusuElement=enesPage.aramaKutusu;
-   Assert.assertTrue(aramaKutusuElement.isDisplayed());
+   enesPage.aramaKutusu.isDisplayed();
    enesPage.aramaKutusu.sendKeys("Rabies Vaccine");
-  WebElement searchButtonElement=enesPage.searchButton;
-  Assert.assertTrue(searchButtonElement.isEnabled());
+  enesPage.searchButton.isEnabled();
    enesPage.searchButton.click();
-   //sonuç bulunabildiği doğrulanr
-   int actualListelenenSonuc=enesPage.listelenenSonuc.size();
-    Assert.assertTrue(actualListelenenSonuc>0);
-    Driver.quitDriver();
 
 
 
