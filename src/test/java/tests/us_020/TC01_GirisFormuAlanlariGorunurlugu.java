3d07cf5b6687d99d78295f57fe138ccd2b9a374d
melosnur <melahatnurozcan@gmail.com>
2025-12-04 15:06:47 +0300

    Melahatnur / 04.12.2025 / 15.00

--- src/test/java/tests/us_020/TC01_GirisFormuAlanlariGorunurlugu.java
+++ src/test/java/tests/us_020/TC01_GirisFormuAlanlariGorunurlugu.java
@@ -1,18 +1,18 @@
 package tests.us_020;
 
 import org.testng.annotations.Test;
-import pages.WadoudPages;
+import pages.WadoudPage;
 import utilities.ConfigReader;
 import utilities.Driver;
 
 public class TC01_GirisFormuAlanlariGorunurlugu {
 
-    WadoudPages pages;
+    WadoudPage pages;
 
     @Test
     public void us_020_TC01_GirisFormuAlanlariGorunurlugu(){
 
-        pages = new WadoudPages();
+        pages = new WadoudPage();
         Driver.getDriver().get(ConfigReader.getProperty("url"));
 
         pages.mainSignInButton.click();
